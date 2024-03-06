07660c285e07e89cdc4114b30150f6085faafc92
mohammadsoheluddin <62943893+mohammadsoheluddin@users.noreply.github.com>
2024-03-06 20:06:38 +0600

    Full Project Done

--- summer-sale.js
+++ summer-sale.js
@@ -1,18 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const closeModalBtn = document.getElementById('go-home');
+
+    closeModalBtn.addEventListener('click', function () {
+        window.location.reload(); // Refresh the page
+    });
+});
+
 function getPrice(priceID) {
     const priceField = document.getElementById(priceID);
     const price = parseFloat(priceField.innerText);
     return price;
 }
 
-document.getElementById('btn-add-1').addEventListener('click', function () {
-    const kAccPrice = getPrice('k-accessories-1-price');
-    const oldTotalPrice = getPrice('total-price');
-    const newTotalPrice = kAccPrice + oldTotalPrice;
-    document.getElementById('total-price').innerText = newTotalPrice.toFixed(2);
-    document.getElementById('total').innerText = newTotalPrice.toFixed(2);
+function productPriceCalculation(productId){
+    const productPrice = getPrice(productId);
+    const productOldTotalPrice = getPrice('total-price');
+    const productNewTotalPrice = productPrice + productOldTotalPrice;
+    document.getElementById('total-price').innerText = productNewTotalPrice.toFixed(2);
+    document.getElementById('total').innerText = productNewTotalPrice.toFixed(2);
 
     const confirmBtn = document.getElementById('confirm');
-    if(newTotalPrice > 0){
+    if(productNewTotalPrice > 0){
         confirmBtn.disabled = false;
     } else{
         confirmBtn.disabled = true;
@@ -21,12 +29,12 @@ document.getElementById('btn-add-1').addEventListener('click', function () {
     const applyButton = document.getElementById('btn-apply');
     const couponCode = document.getElementById('coupon-code');
 
-    if (newTotalPrice >= 200) {
+    if (productNewTotalPrice >= 200) {
         applyButton.disabled = false;
         applyButton.addEventListener('click', function () {
             if (couponCode.value === 'SELL200') {
-                const discount = newTotalPrice * 0.2;
-                const grandTotal = newTotalPrice - discount;
+                const discount = productNewTotalPrice * 0.2;
+                const grandTotal = productNewTotalPrice - discount;
                 document.getElementById('discount').innerText = discount.toFixed(2);
                 document.getElementById('total').innerText = grandTotal.toFixed(2);
             }
@@ -34,16 +42,64 @@ document.getElementById('btn-add-1').addEventListener('click', function () {
     } else {
         applyButton.disabled = true;
     }
+}
+
+document.getElementById('btn-add-1').addEventListener('click', function () {
+    productPriceCalculation('product-card-1')
+});
+
+document.getElementById('btn-add-2').addEventListener('click', function () {
+    productPriceCalculation('product-card-2')
+});
+
+document.getElementById('btn-add-3').addEventListener('click', function () {
+    productPriceCalculation('product-card-3')
+});
+
+document.getElementById('btn-add-4').addEventListener('click', function () {
+    productPriceCalculation('product-card-4')
+});
+
+document.getElementById('btn-add-5').addEventListener('click', function () {
+    productPriceCalculation('product-card-5')
+});
+
+document.getElementById('btn-add-6').addEventListener('click', function () {
+    productPriceCalculation('product-card-6')
+});
+
+document.getElementById('btn-add-7').addEventListener('click', function () {
+    productPriceCalculation('product-card-7')
+});
+
+document.getElementById('btn-add-8').addEventListener('click', function () {
+    productPriceCalculation('product-card-8')
+});
+
+document.getElementById('btn-add-9').addEventListener('click', function () {
+    productPriceCalculation('product-card-9')
 });
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    const closeModalBtn = document.getElementById('go-home');
 
-    closeModalBtn.addEventListener('click', function () {
-        window.location.reload(); // Refresh the page
-    });
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
